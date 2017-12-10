5df4128f1d4d872a33fa77b4b0da88958f5be824
alpbourne <alpbourne@gmail.com>
2017-12-10 22:13:43 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,19 @@
+class MyValidator < ActiveRecord::Validator
+  def validate(record)
+    if record.title
+      words = ["Top [number]", "Guess", "Won't Believe", "Secret"]
+      unless words.any? {|w| record.title.include?(w)}
+        record.errors[:base] << (options[:message] || "title is not clickbaity")
+      end
+    end
+    false
+  end
+end
+
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
+
